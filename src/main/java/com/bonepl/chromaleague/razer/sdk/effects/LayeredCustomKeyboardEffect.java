28f37d8cafb072aee2a71e59897136134de5cd33
BonE <bone.pl@gmail.com>
2020-10-10 20:56:38 +0200

    Added Keyboard Effect for combining Custom effects

---
+++ src/main/java/com/bonepl/chromaleague/razer/sdk/effects/LayeredCustomKeyboardEffect.java
@@ -0,0 +1,18 @@
+package com.bonepl.chromaleague.razer.sdk.effects;
+
+public class LayeredCustomKeyboardEffect extends CustomKeyboardEffect {
+
+    public void addCustomKeyboardEffect(CustomKeyboardEffect customKeyboardEffect) {
+        for (int i = 0; i < colors.length; i++) {
+            if (customKeyboardEffect.colors[i] != Color.BLACK.getSDKColorRef()) {
+                colors[i] = customKeyboardEffect.colors[i];
+            }
+        }
+    }
+
+    public LayeredCustomKeyboardEffect withCustomKeyboardEffect(CustomKeyboardEffect customKeyboardEffect) {
+        addCustomKeyboardEffect(customKeyboardEffect);
+        return this;
+    }
+
+}
--- src/test/java/com/bonepl/chromaleague/razer/sdk/effects/ProgressBarKeyboardEffectTest.java
+++ src/test/java/com/bonepl/chromaleague/razer/sdk/effects/ProgressBarKeyboardEffectTest.java
@@ -8,24 +8,22 @@ import java.util.Arrays;
 import java.util.List;
 
 class ProgressBarKeyboardEffectTest {
+    public static final List<RzKey> BLACKWIDOW_FIRST_ROW = Arrays.asList(RzKey.RZKEY_ESC, RzKey.RZKEY_F1, RzKey.RZKEY_F2, RzKey.RZKEY_F3, RzKey.RZKEY_F4,
+            RzKey.RZKEY_F5, RzKey.RZKEY_F6, RzKey.RZKEY_F7, RzKey.RZKEY_F8, RzKey.RZKEY_F9,
+            RzKey.RZKEY_F10, RzKey.RZKEY_F11, RzKey.RZKEY_F12, RzKey.RZKEY_PRINTSCREEN,
+            RzKey.RZKEY_SCROLL, RzKey.RZKEY_PAUSE);
+
     @Test
     void testProgressBarEffect() throws InterruptedException {
-        //given
-        List<RzKey> blackWidowFirstRow = Arrays.asList(RzKey.RZKEY_ESC, RzKey.RZKEY_F1, RzKey.RZKEY_F2, RzKey.RZKEY_F3, RzKey.RZKEY_F4,
-                RzKey.RZKEY_F5, RzKey.RZKEY_F6, RzKey.RZKEY_F7, RzKey.RZKEY_F8, RzKey.RZKEY_F9,
-                RzKey.RZKEY_F10, RzKey.RZKEY_F11, RzKey.RZKEY_F12, RzKey.RZKEY_PRINTSCREEN,
-                RzKey.RZKEY_SCROLL, RzKey.RZKEY_PAUSE);
-
-        //then
         try (RazerSDKClient razerSDKClient = new RazerSDKClient()) {
             for (int i = 0; i <= 100; i += 5) {
                 razerSDKClient.createKeyboardEffect(
-                        new ProgressBarKeyboardEffect(blackWidowFirstRow, i, Color.GREEN));
+                        new ProgressBarKeyboardEffect(BLACKWIDOW_FIRST_ROW, i, Color.GREEN));
                 Thread.sleep(100);
             }
             for (int i = 100; i >= 0; i -= 5) {
                 razerSDKClient.createKeyboardEffect(
-                        new ProgressBarKeyboardEffect(blackWidowFirstRow, i, Color.GREEN));
+                        new ProgressBarKeyboardEffect(BLACKWIDOW_FIRST_ROW, i, Color.GREEN));
                 Thread.sleep(100);
             }
         }

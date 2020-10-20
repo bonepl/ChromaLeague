7d1a57b17cc2984eac0a490fd755e728a189268b
Jakub Andrzejewski <72575496+bonepl@users.noreply.github.com>
2020-10-20 22:01:53 +0200

    added pulsing background on dead (#31)

--- chroma-league/src/main/java/com/bonepl/chromaleague/hud/parts/Background.java
+++ chroma-league/src/main/java/com/bonepl/chromaleague/hud/parts/Background.java
@@ -1,6 +1,7 @@
 package com.bonepl.chromaleague.hud.parts;
 
 import com.bonepl.chromaleague.GameStateHelper;
+import com.bonepl.chromaleague.hud.BreathingColor;
 import com.bonepl.chromaleague.hud.animations.BaronBuffBackground;
 import com.bonepl.razersdk.animation.AnimatedFrame;
 import com.bonepl.razersdk.animation.Color;
@@ -10,9 +11,14 @@ import com.bonepl.razersdk.animation.LayeredFrame;
 public class Background extends LayeredFrame {
     public static final Color BACKGROUND_COLOR = new Color(10, 10, 10);
     private static final AnimatedFrame baronBuffBackground = new BaronBuffBackground();
+    private static final BreathingColor deadBackground = new BreathingColor(new Color(100, 0, 0));
 
     public Background() {
-        addFrame(new Frame(BACKGROUND_COLOR));
+        if (GameStateHelper.isActivePlayerAlive()) {
+            addFrame(new Frame(BACKGROUND_COLOR));
+        } else {
+            addFrame(new Frame(deadBackground.getNextColor()));
+        }
         if (GameStateHelper.hasBaronBuff()) {
             addFrame(baronBuffBackground.getFrame());
         }

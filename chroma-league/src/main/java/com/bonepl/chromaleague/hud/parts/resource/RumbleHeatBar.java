fba26ca93b48274398e1705603c80389c38f7a34
BonE <bone.pl@gmail.com>
2020-11-02 23:27:40 +0100

    rumble test

--- chroma-league/src/main/java/com/bonepl/chromaleague/hud/parts/resource/RumbleHeatBar.java
+++ chroma-league/src/main/java/com/bonepl/chromaleague/hud/parts/resource/RumbleHeatBar.java
@@ -6,35 +6,59 @@ import com.bonepl.chromaleague.tasks.MainTask;
 import com.bonepl.razersdk.animation.AnimatedFrame;
 import com.bonepl.razersdk.animation.Color;
 import com.bonepl.razersdk.animation.Frame;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.Duration;
+import java.time.LocalTime;
 
 public class RumbleHeatBar extends AnimatedFrame {
+    private static final Logger LOGGER = LogManager.getLogger();
     private static final double HEAT_THRESHOLD = 13.0 * MainTask.ACTIVE_PLAYER_FETCH_DELAY / 1000;
-    private int previousResourcePercentage;
+    //    private static Lo
+    private static int previousResourcePercentage = 0;
+    private static LocalTime previousResourceChange = LocalTime.now();
+    private static boolean overheating = false;
 
     @Override
     public Frame getFrame() {
         final int resourcePercentage = GameStateHelper.getResourcePercentage();
+//        LOGGER.info("resp " + resourcePercentage);
+//        LOGGER.info(diff + " to threshold " + HEAT_THRESHOLD);
+        if (resourcePercentage != previousResourcePercentage) {
+            final LocalTime now = LocalTime.now();
+            if (resourcePercentage == 100) {
+                overheating = true;
+            } else if (resourcePercentage < previousResourcePercentage) {
+                final long durationOfChange = Duration.between(previousResourceChange, now).toMillis();
+                overheating = durationOfChange < 650;
+                LOGGER.info("From " + previousResourcePercentage + " to " + resourcePercentage + " Duration: " + durationOfChange + " Overheating: " + overheating);
+            } else {
+                overheating = false;
+            }
+            previousResourceChange = now;
+        }
+
         Color color;
-        if (resourcePercentage >= previousResourcePercentage) {
+        if (resourcePercentage > previousResourcePercentage) {
             if (resourcePercentage >= 50) {
                 color = Color.YELLOW;
             } else {
                 color = Color.WHITE;
             }
         } else {
-            final int diff = previousResourcePercentage - resourcePercentage;
-            if (diff < HEAT_THRESHOLD) {
+            if (overheating) {
+                color = Color.RED;
+            } else {
                 if (resourcePercentage >= 50) {
                     color = Color.YELLOW;
                 } else {
                     color = Color.WHITE;
                 }
-            } else {
-                color = Color.RED;
             }
         }
-        addAnimationFrame(new ProgressBar(ResourceBars.getResourceBarKeys(), resourcePercentage, color));
         previousResourcePercentage = resourcePercentage;
+        addAnimationFrame(new ProgressBar(ResourceBars.getResourceBarKeys(), resourcePercentage, color));
         return super.getFrame();
     }
 }

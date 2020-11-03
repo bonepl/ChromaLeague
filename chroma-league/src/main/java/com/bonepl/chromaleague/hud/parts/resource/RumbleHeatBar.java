32b92ef90b06550bc837e60fd30cc20b1883d96d
Krzysztof Wacławik <krzysztof.waclawik.kao@gmail.com>
2020-11-03 10:49:34 +0100

    KW rumble idea logic - refactor or revert if that's of some use. Still needs probably more tests to properly cleanup state after cooldown or such

--- chroma-league/src/main/java/com/bonepl/chromaleague/hud/parts/resource/RumbleHeatBar.java
+++ chroma-league/src/main/java/com/bonepl/chromaleague/hud/parts/resource/RumbleHeatBar.java
@@ -20,12 +20,54 @@ public class RumbleHeatBar extends AnimatedFrame {
     private static LocalTime previousResourceChange = LocalTime.now();
     private static boolean overheating = false;
 
+    private static LocalTime lastTimeSeeingRise = LocalTime.now();
+    private static int highestValue = 0;
+    boolean falling = false;
+
     @Override
     public Frame getFrame() {
         final int resourcePercentage = GameStateHelper.getResourcePercentage();
 //        LOGGER.info("resp " + resourcePercentage);
 //        LOGGER.info(diff + " to threshold " + HEAT_THRESHOLD);
         if (resourcePercentage != previousResourcePercentage) {
+            if (resourcePercentage < previousResourcePercentage) {
+                // we're falling
+                if (!falling) {
+                    // note the last time we saw an increase;
+                    lastTimeSeeingRise = previousResourceChange;
+                    highestValue = previousResourcePercentage;
+                }
+                falling = true;
+            }
+            if (resourcePercentage > previousResourcePercentage) {
+                // we're rising
+                falling = false;
+            }
+            // wylicz pochodną aka oblicz spadek w przedziale czasu.
+            // if spadek > jakaś wartość - overheat
+            // otherwise - casual cooling down.
+            // wykorzystaj że w wiekszym przedziale czasu niz jeden tick wynik sie uspójni, niezaleznie od tick rate
+            if (falling) {
+                final LocalTime now = LocalTime.now();
+                final long durationOfChange = Duration.between(lastTimeSeeingRise, now).toMillis();
+
+                final int delta = highestValue - resourcePercentage;
+                double magicNumber = delta * 1000.0 / durationOfChange;
+                System.out.println(magicNumber);
+                // rumble cooldown when overheat is 16, but let's have some margin
+                if (magicNumber > 13.0) {
+                    System.out.println("KW says - rumble overheats");
+                    // color red and pulse or whatever
+                } else {
+                    // rumble is cooling down normally - ~10 heat per sec, 9.6 according to math in this program :D
+                    System.out.println("KW says - rumble cools down normally");
+                    // color according to heat status
+                }
+            } else {
+                System.out.println("KW says - rumble dances in the fountain");
+                // color according to heat status
+            }
+
             final LocalTime now = LocalTime.now();
             if (resourcePercentage == 100) {
                 overheating = true;

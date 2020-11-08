ca78fe32d6f871a44a8ab5a5ce9729170ee84ce7
BonE <bone.pl@gmail.com>
2020-11-08 21:20:44 +0100

    logger update

--- chroma-league/src/main/java/com/bonepl/chromaleague/tasks/FetchGameStats.java
+++ chroma-league/src/main/java/com/bonepl/chromaleague/tasks/FetchGameStats.java
@@ -3,12 +3,12 @@ package com.bonepl.chromaleague.tasks;
 import com.bonepl.chromaleague.rest.LeagueHttpClient;
 import com.bonepl.chromaleague.rest.gamestats.GameStats;
 import com.jsoniter.JsonIterator;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import java.util.logging.Logger;
 
 public class FetchGameStats {
     public static final String URL = "https://127.0.0.1:2999/liveclientdata/gamestats";
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = Logger.getLogger(FetchGameStats.class.getName());
 
     public GameStats fetchGameStats() {
         return LeagueHttpClient.getResponse(URL)

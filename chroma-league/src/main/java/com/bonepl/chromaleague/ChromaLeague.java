92c2909ffc647c4ca7fe3360cbf4b612b883c6a6
BonE <bone.pl@gmail.com>
2020-11-15 11:44:15 +0100

    added press enter to exit

--- chroma-league/src/main/java/com/bonepl/chromaleague/ChromaLeague.java
+++ chroma-league/src/main/java/com/bonepl/chromaleague/ChromaLeague.java
@@ -4,10 +4,11 @@ import com.bonepl.chromaleague.rest.LeagueHttpClient;
 import com.bonepl.chromaleague.tasks.CheckRiotApiTask;
 import com.bonepl.chromaleague.tasks.MainTask;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ChromaLeague implements AutoCloseable {
@@ -15,21 +16,12 @@ public class ChromaLeague implements AutoCloseable {
     private final ScheduledExecutorService mainExecutorService = Executors.newScheduledThreadPool(5);
 
     public void runChromaLeague() {
-        LOGGER.info("Started Chroma League");
+        LOGGER.info("Started Chroma League - press ENTER to exit");
         mainExecutorService.scheduleWithFixedDelay(new CheckRiotApiTask(), 0, 1000, TimeUnit.MILLISECONDS);
         mainExecutorService.scheduleWithFixedDelay(new MainTask(), 100, 500, TimeUnit.MILLISECONDS);
-        while (isAlive()) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                LOGGER.log(Level.WARNING, e, () -> "Main ChromaLeague thread interrupted");
-                Thread.currentThread().interrupt();
-            }
+        try (Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8)) {
+            scanner.nextLine();
         }
-    }
-
-    private static boolean isAlive() {
-        return true;
     }
 
     @Override

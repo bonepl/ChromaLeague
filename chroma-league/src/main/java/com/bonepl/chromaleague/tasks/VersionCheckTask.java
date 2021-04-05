cac6a55deaa0fcba7f62964971b16b1eec417f5c
BonE <bone.pl@gmail.com>
2021-04-05 23:01:35 +0200

    version checker added

--- chroma-league/src/main/java/com/bonepl/chromaleague/tasks/VersionCheckTask.java
+++ chroma-league/src/main/java/com/bonepl/chromaleague/tasks/VersionCheckTask.java
@@ -1,24 +1,23 @@
 package com.bonepl.chromaleague.tasks;
 
-import java.io.BufferedReader;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class VersionCheckTask implements Runnable {
     private static final String ONLINE_VERSION_URL = "https://raw.githubusercontent.com/bonepl/ChromaLeague/master/chroma-league/src/main/resources/VERSION";
     private static final String LATEST_RELEASE_URL = "https://github.com/bonepl/ChromaLeague/releases/latest";
-    private static final String LOCAL_VERSION_PATH = "VERSION";
+    private static final String LOCAL_VERSION_PATH = "/VERSION";
     private static final Logger LOGGER = Logger.getLogger(VersionCheckTask.class.getName());
 
     @Override
@@ -28,30 +27,32 @@ public class VersionCheckTask implements Runnable {
             final List<String> localVersion = getLocalVersion();
             LOGGER.info(() -> "Running ChromaLeague version " + localVersion.get(0));
             if (newerVersionAvailable(localVersion.get(0), onlineVersion.get(0))) {
-                LOGGER.info(() -> getNewerVersionMessage(onlineVersion));
+                LOGGER.info(() -> "There is a newer ChromaLeague version " + onlineVersion.get(0) + " available!");
+                LOGGER.info("New changes:");
+                IntStream.range(1, onlineVersion.size())
+                        .mapToObj(onlineVersion::get)
+                        .forEach(LOGGER::info);
+                LOGGER.info(() -> "Please go to " + LATEST_RELEASE_URL + " to get the newest version!");
             } else {
                 LOGGER.info(() -> "Your ChromaLeague version is up to date!");
             }
-        } catch (IOException | URISyntaxException ex) {
+        } catch (IOException ex) {
             LOGGER.log(Level.WARNING, ex, () -> "Couldn't check ChromaLeague version");
         }
     }
 
-    private static String getNewerVersionMessage(List<String> onlineVersion) {
-        return "There is a newer ChromaLeague version " + onlineVersion.get(0) + " available!" + System.lineSeparator()
-                + "New changes:" + System.lineSeparator()
-                + IntStream.range(1, onlineVersion.size()).mapToObj(onlineVersion::get).collect(Collectors.joining(System.lineSeparator())) + System.lineSeparator()
-                + "Please go to " + LATEST_RELEASE_URL + " to get the newest version!";
-    }
-
     public List<String> getOnlineVersion() throws IOException {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(ONLINE_VERSION_URL).openStream(), StandardCharsets.UTF_8))) {
-            return bufferedReader.lines().collect(Collectors.toList());
+        try (final CloseableHttpClient defaultHttpClient = HttpClients.createDefault()) {
+            return Arrays.asList(EntityUtils.toString(
+                    defaultHttpClient.execute(new HttpGet(ONLINE_VERSION_URL)).getEntity())
+                    .split(System.lineSeparator()));
         }
     }
 
-    public List<String> getLocalVersion() throws IOException, URISyntaxException {
-        return Files.readAllLines(Paths.get(VersionCheckTask.class.getClassLoader().getResource(LOCAL_VERSION_PATH).toURI()));
+    public List<String> getLocalVersion() throws IOException {
+        try (final InputStream resourceAsStream = getClass().getResourceAsStream(LOCAL_VERSION_PATH)) {
+            return Arrays.asList(new String(resourceAsStream.readAllBytes(), StandardCharsets.UTF_8).split(System.lineSeparator()));
+        }
     }
 
     public boolean newerVersionAvailable(String currentVersion, String onlineVersion) {

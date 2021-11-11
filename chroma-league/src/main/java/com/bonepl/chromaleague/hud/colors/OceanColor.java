53df0dfe841246185a34e416dd36428bd45a2724
BonE <bone.pl@gmail.com>
2021-11-11 18:30:06 +0100

    ocean adjustments

--- chroma-league/src/main/java/com/bonepl/chromaleague/hud/colors/OceanColor.java
+++ chroma-league/src/main/java/com/bonepl/chromaleague/hud/colors/OceanColor.java
@@ -7,10 +7,11 @@ import com.bonepl.razersdk.color.StaticColor;
 import java.security.SecureRandom;
 
 public class OceanColor implements Color {
-    MultiTransitionColor oceanColor = new MultiTransitionColor.Builder(new StaticColor(0, 0, 80))
-            .addTransition(StaticColor.BLUE, 10)
-            .addTransition(new StaticColor(0, 0, 80), 10)
-            .addTransition(StaticColor.BLUE, 10)
+    private static final StaticColor DARKER_OCEAN = new StaticColor(0, 60, 60);
+    MultiTransitionColor oceanColor = new MultiTransitionColor.Builder(DARKER_OCEAN)
+            .addTransition(CLColor.OCEAN, 10)
+            .addTransition(DARKER_OCEAN, 10)
+            .addTransition(CLColor.OCEAN, 10)
             .addTransition(new StaticColor(30, 40, 40), 5)
             .looped(5)
             .build();

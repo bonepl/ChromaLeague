ce0b0db110b07e5112ae12d8d6001ed59313d4ab
BonE <bone.pl@gmail.com>
2021-11-20 19:18:10 +0100

    correction to baron buff area

--- chroma-league/src/main/java/com/bonepl/chromaleague/hud/animations/BaronBuffBackgroundAnimation.java
+++ chroma-league/src/main/java/com/bonepl/chromaleague/hud/animations/BaronBuffBackgroundAnimation.java
@@ -1,11 +1,11 @@
 package com.bonepl.chromaleague.hud.animations;
 
 import com.bonepl.chromaleague.hud.colors.BackgroundBreathingColor;
-import com.bonepl.razersdk.color.BreathingColor;
 import com.bonepl.razersdk.animation.AnimatedFrame;
-import com.bonepl.razersdk.color.StaticColor;
 import com.bonepl.razersdk.animation.Frame;
 import com.bonepl.razersdk.animation.SimpleFrame;
+import com.bonepl.razersdk.color.BreathingColor;
+import com.bonepl.razersdk.color.StaticColor;
 import com.bonepl.razersdk.sdk.RzKey;
 import com.bonepl.razersdk.sdk.RzKeyJoiner;
 import com.bonepl.razersdk.sdk.RzKeySelector;
@@ -37,7 +37,6 @@ public class BaronBuffBackgroundAnimation extends AnimatedFrame {
                 .with(new RzKeySelector().withRowOf(RZKEY_Q).withColumnBetween(RZKEY_Q, RZKEY_U))
                 .with(new RzKeySelector().withRowOf(RZKEY_A).withColumnBetween(RZKEY_A, RZKEY_H))
                 .with(new RzKeySelector().withRowOf(RZKEY_Z).withColumnBetween(RZKEY_Z, RZKEY_B))
-                .with(new RzKeySelector().withRowOf(RZKEY_LALT).withColumnBetween(RZKEY_LALT, RZKEY_SPACE))
                 .join();
     }
 }

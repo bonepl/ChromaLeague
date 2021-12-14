f39eff1e953d17a9061ec588bd2b6cc28d19e3fd
BonE <bone.pl@gmail.com>
2021-12-14 21:39:09 +0100

    tests and refactoring

--- chroma-league/src/test/java/com/bonepl/chromaleague/rest/eventdata/EventTypeTest.java
+++ chroma-league/src/test/java/com/bonepl/chromaleague/rest/eventdata/EventTypeTest.java
@@ -5,6 +5,11 @@ import com.bonepl.chromaleague.ResourceLoader;
 import com.bonepl.chromaleague.hud.parts.dragons.KilledDragonsBarTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -18,8 +23,41 @@ class EventTypeTest {
         gameStateMocks = new GameStateMocks();
     }
 
+    @ParameterizedTest(name = "{0} -> {1}")
+    @MethodSource("dragonToExpectedTypeArguments")
+    void testDragonsAnimation(DragonType dragonType, EventType expectedEventType, boolean alliedKill) {
+        //given
+        when(gameStateMocks.playerList().isAlly(any())).thenReturn(alliedKill);
+        final Event event = KilledDragonsBarTest.getEventForDragonKill(dragonType);
+
+        //when
+        final EventType eventType = EventType.fromEvent(event);
+
+        //then
+        assertEquals(expectedEventType, eventType);
+    }
+
+    static Stream<Arguments> dragonToExpectedTypeArguments() {
+        return Stream.of(
+                Arguments.of(DragonType.OCEAN, EventType.ALLY_OCEAN_DRAGON_KILL, true),
+                Arguments.of(DragonType.MOUNTAIN, EventType.ALLY_MOUNTAIN_DRAGON_KILL, true),
+                Arguments.of(DragonType.INFERNAL, EventType.ALLY_INFERNAL_DRAGON_KILL, true),
+                Arguments.of(DragonType.CLOUD, EventType.ALLY_CLOUD_DRAGON_KILL, true),
+                Arguments.of(DragonType.CHEMTECH, EventType.ALLY_CHEMTECH_DRAGON_KILL, true),
+                Arguments.of(DragonType.HEXTECH, EventType.ALLY_HEXTECH_DRAGON_KILL, true),
+                Arguments.of(DragonType.ELDER, EventType.ALLY_ELDER_DRAGON_KILL, true),
+                Arguments.of(DragonType.OCEAN, EventType.ENEMY_OCEAN_DRAGON_KILL, false),
+                Arguments.of(DragonType.MOUNTAIN, EventType.ENEMY_MOUNTAIN_DRAGON_KILL, false),
+                Arguments.of(DragonType.INFERNAL, EventType.ENEMY_INFERNAL_DRAGON_KILL, false),
+                Arguments.of(DragonType.CLOUD, EventType.ENEMY_CLOUD_DRAGON_KILL, false),
+                Arguments.of(DragonType.CHEMTECH, EventType.ENEMY_CHEMTECH_DRAGON_KILL, false),
+                Arguments.of(DragonType.HEXTECH, EventType.ENEMY_HEXTECH_DRAGON_KILL, false),
+                Arguments.of(DragonType.ELDER, EventType.ENEMY_ELDER_DRAGON_KILL, false)
+        );
+    }
+
     @Test
-    void testActivePLayerKill() {
+    void testActivePlayerKill() {
         //given
         when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
         final Event event = ResourceLoader.eventFromJson("activePlayerKill.json");
@@ -32,7 +70,7 @@ class EventTypeTest {
     }
 
     @Test
-    void testActivePLayerAssist() {
+    void testActivePlayerAssist() {
         //given
         when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
         final Event event = ResourceLoader.eventFromJson("activePlayerAssist.json");
@@ -45,189 +83,7 @@ class EventTypeTest {
     }
 
     @Test
-    void testAllyOceanDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.OCEAN);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ALLY_OCEAN_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testAllyInfernalDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.INFERNAL);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ALLY_INFERNAL_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testAllyCloudDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.CLOUD);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ALLY_CLOUD_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testAllyMountainDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.MOUNTAIN);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ALLY_MOUNTAIN_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testAllyChemtechDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.CHEMTECH);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ALLY_CHEMTECH_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testAllyHextechDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.HEXTECH);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ALLY_HEXTECH_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testAllyElderDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.ELDER);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ALLY_ELDER_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testEnemyOceanDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(false);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.OCEAN);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ENEMY_OCEAN_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testEnemyInfernalDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(false);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.INFERNAL);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ENEMY_INFERNAL_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testEnemyCloudDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(false);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.CLOUD);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ENEMY_CLOUD_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testEnemyMountainDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(false);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.MOUNTAIN);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ENEMY_MOUNTAIN_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testEnemyChemtechDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(false);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.CHEMTECH);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ENEMY_CHEMTECH_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testEnemyHextechDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(false);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.HEXTECH);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ENEMY_HEXTECH_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testEnemyElderDragonAnimation() {
-        //given
-        when(gameStateMocks.playerList().isAlly(any())).thenReturn(false);
-        final Event event = KilledDragonsBarTest.getEventForDragonKill(DragonType.ELDER);
-
-        //when
-        final EventType eventType = EventType.fromEvent(event);
-
-        //then
-        assertEquals(EventType.ENEMY_ELDER_DRAGON_KILL, eventType);
-    }
-
-    @Test
-    void testAllyHeraldAnimation() {
+    void testAllyHeraldKillEvent() {
         //given
         when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
         final Event event = ResourceLoader.eventFromJson("allyHeraldKill.json");
@@ -240,7 +96,7 @@ class EventTypeTest {
     }
 
     @Test
-    void testEnemyHeraldAnimation() {
+    void testEnemyHeraldKillEvent() {
         //given
         when(gameStateMocks.playerList().isAlly(any())).thenReturn(false);
         final Event event = ResourceLoader.eventFromJson("allyHeraldKill.json");
@@ -253,7 +109,7 @@ class EventTypeTest {
     }
 
     @Test
-    void testAllyBaronAnimation() {
+    void testAllyBaronKillEvent() {
         //given
         when(gameStateMocks.playerList().isAlly(any())).thenReturn(true);
         final Event event = ResourceLoader.eventFromJson("allyBaronKill.json");
@@ -266,7 +122,7 @@ class EventTypeTest {
     }
 
     @Test
-    void testEnemyBaronAnimation() {
+    void testEnemyBaronKillEvent() {
         //given
         when(gameStateMocks.playerList().isAlly(any())).thenReturn(false);
         final Event event = ResourceLoader.eventFromJson("allyBaronKill.json");
@@ -279,7 +135,7 @@ class EventTypeTest {
     }
 
     @Test
-    void testGameEndWin() {
+    void testGameEndWinEvent() {
         //given
         final Event event = ResourceLoader.eventFromJson("gameEndWin.json");
 
@@ -291,7 +147,7 @@ class EventTypeTest {
     }
 
     @Test
-    void testGameEndLose() {
+    void testGameEndLoseEvent() {
         //given
         final Event event = ResourceLoader.eventFromJson("gameEndLose.json");
 

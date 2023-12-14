3576918299860787b861298b68f894830d90467e
Jakub BonE Andrzejewski <bone.pl@gmail.com>
2023-12-14 21:25:20 +0100

    pom versions bump

--- chroma-league-launcher/src/scripts/generate-cl-jre.bat
+++ chroma-league-launcher/src/scripts/generate-cl-jre.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-%JAVA_HOME%\bin\jlink --module-path "%JAVA_HOME%\jmods" --compress=2 --add-modules java.base,java.logging,java.naming,jdk.crypto.ec --no-header-files --no-man-pages --output .\target\cl-jre
+%JAVA_HOME%\bin\jlink --module-path "%JAVA_HOME%\jmods" --compress=zip-6 --add-modules java.base,java.logging,java.naming,jdk.crypto.ec --no-header-files --no-man-pages --output .\target\cl-jre
